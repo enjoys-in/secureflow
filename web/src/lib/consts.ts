d27b91d1e3ca85a050b337138cc5cf071f5f5e0b
Mullayam singh <mullayam06@gmail.com>
2026-02-14 23:17:38 +0530

    docker update and build in vite

--- web/src/lib/consts.ts
+++ web/src/lib/consts.ts
@@ -1,5 +1,7 @@
 
+const APP_ENV = import.meta.env.VITE_APP_ENV || "development"
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8443/api/v1"
+export const IS_PRODUCTION = APP_ENV === "PRODUCTION" || APP_ENV === "production" || APP_ENV === "prod" || APP_ENV === "PROD"
+export const API_BASE_URL =  IS_PRODUCTION ? import.meta.env.VITE_API_URL : "http://localhost:8443/api/v1"
 
 export const IMMUTABLE_PORTS = (import.meta.env.VITE_IMMUTABLE_PORTS || "22,25,465,587,3306,6379").split(",").map((p: string) => parseInt(p.trim(), 10))
