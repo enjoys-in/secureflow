af06615aa1752f1f1ee43f7b6e9a11e1d5368ae2
Mullayam singh <mullayam06@gmail.com>
2026-03-20 19:48:50 +0530

    Add New Sh

--- independent.sh
+++ independent.sh
@@ -1,5 +1,5 @@
 
-docker compose --env-file ./web/.env.prod -f docker-compose-prod.yml build --no-cache web
+docker compose --env-file ./web/.env -f docker-compose-prod.yml build --no-cache web
 
 # Run
-docker compose --env-file ./web/.env.prod -f docker-compose-prod.yml up -d web --no-deps
+docker compose --env-file ./web/.env -f docker-compose-prod.yml up -d web --no-deps
